d4ef478898d8897f95aa344442e152f2414d337e
Basil Hariri <basilhariri@utexas.edu>
2015-07-12 21:59:41 -0700

    made UI

--- bootstrap/index.php
+++ bootstrap/index.php
@@ -26,40 +26,81 @@
 
 
   <!-- CSS -->
-  <link rel="stylesheet" type="text/css" href="thebeginning.css">
+  <link rel="stylesheet" type="text/css" href="index.css">
 
 
 </head>
 <body>
-
+  <nav class="navbar navbar-default navbar-static-top">
+    <div class="container">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a href="#" class="navbar-brand"><img id="logo" src="http://i.ytimg.com/vi/p1Pt7t2UEPc/maxresdefault.jpg" alt="some_text"></a>
+      </div>
+      <!-- Collection of nav links, forms, and other content for toggling -->
+      <div id="navbar" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li><a href="#submit">Submit</a></li>
+          <li><a href="#about">About</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <form role="search" class="navbar-form navbar-left">
+            <div class="form-group">
+              <input type="text" placeholder="email@example.com" class="form-control">
+            </div>
+          </form>
+          <li><a href="#">Subscribe</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <br>
   <div class="container">
     <ul class="nav nav-tabs">
-      <li class="active"><a href="#video" data-toggle="tab">Film</a></li>
-      <li><a href="#music" data-toggle="tab">Music</a></li>
+      <li class="active"><a href="#video" data-toggle="tab"><span class="glyphicon glyphicon-film"></span> Film</a></li>
+      <li><a href="#music" data-toggle="tab"><span class="glyphicon glyphicon-music"></span> Music</a></li>
     </ul>
   </div>
 
   <div class="tab-content">
     <div id="video" class="tab-pane fade in active">
       <div id="showcase">
-
-        <video id="showcase_video" class="video-js vjs-default-skin"
-        controls preload="auto" width="640" height="264"
-        poster="http://video-js.zencoder.com/oceans-clip.png"
-        data-setup='{"example_option":true}'>
-        <source src="http://video-js.zencoder.com/oceans-clip.mp4" type='video/mp4' />
-        <source src="http://video-js.zencoder.com/oceans-clip.webm" type='video/webm' />
-        <source src="http://video-js.zencoder.com/oceans-clip.ogv" type='video/ogg' />
-        <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
+        <video id="showcase_video" class="video-js vjs-default-skin" controls preload="auto" poster="http://video-js.zencoder.com/oceans-clip.png" data-setup='{"example_option":true}'>
+          <source src="http://video-js.zencoder.com/oceans-clip.mp4" type='video/mp4'/>
+          <source src="http://video-js.zencoder.com/oceans-clip.webm" type='video/webm' />
+          <source src="http://video-js.zencoder.com/oceans-clip.ogv" type='video/ogg' />
+          <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
         </video>
-
+      </div>
+    </div>
+    <div id="music" class="tab-pane fade">
+      <h3>Music</h3>
+      <p>Some content in menu 1.</p>
     </div>
   </div>
-  <div id="music" class="tab-pane fade">
-    <h3>Music</h3>
-    <p>Some content in menu 1.</p>
-  </div>
-</div>
+<script type="text/javascript">
+  // Once the video is ready
+  _V_("showcase_video").ready(function(){
 
+    var myPlayer = this;    // Store the video object
+    var aspectRatio = 9/16; // Make up an aspect ratio
+
+    function resizeVideoJS(){
+      // Get the parent element's actual width
+      var width = document.getElementById(myPlayer.id).parentElement.offsetWidth;
+      // Set width to fill parent element, Set height
+      myPlayer.width(width).height( width * aspectRatio );
+    }
+
+    resizeVideoJS(); // Initialize the function
+    window.onresize = resizeVideoJS; // Call the function on resize
+  });
+</script>
 </body>
 </html>
